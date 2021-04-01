492f2041b96644d7d4b9f2384eef2aa2b35dfa1f
zwen <zwen000@citymail.cuny.edu>
2021-04-01 15:40:16 -0400

    update

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
 
 posts = [
     {
@@ -27,7 +30,42 @@ def home():
 
 @app.route('/about/')
 def about():
-    return render_template("about.html", title="About")
+    team = [
+        {
+            'name': 'Chrystal Mingo',
+            'image': url_for('static', filename="profile_pics/CM.png"),
+            'description': "is a senior majoring in Computer Science at The City College of New York. She has interned at various companies such as Verizon, Morgan Stanley, and Citi as a software developer. She also has a passion for teaching and has worked as a Teacher for the Girls Who Code Summer Immersion Program, and taught Algebra I and II, as well as Intro and AP Computer Science courses in Spanish and English at Gregorio Luperon High School. Chrystal Mingo is also a Grace Hopper 2019 Scholarship Recipient, and current President of Women in Computer. She will be graduating from CCNY in Spring 2021 and entering Citi’s EIOT full-time rotational program as a project manager. She is one of the frontend developers for AdviseMe and believes this project will be a game-changer for advisement at CCNY.",
+            'git': 'https://github.com/chrystalmingo',
+            'linkedin': '',
+            'email':''
+        },
+        {
+            'name': 'Zhicong Wen',
+            'image': url_for('static', filename="profile_pics/ZW.jpg"),
+            'description': "is a senior majoring in Computer Science at City College of New York. Previously he was interning as a Python programmer at the NYC Department of Environmental Protection(DEP). He also worked on Quality Assurance of the New York City sewer system database. He will be graduating from CCNY in Fall 2021. He is one of the frontend developers for AdviseMe.",
+            'git': 'https://github.com/zwen000',
+            'linkedin': 'https://www.linkedin.com/in/zhicongw-b243a7169/',
+            'email': ''
+        },
+        {
+            'name': 'Xunshan Lin',
+            'image': url_for('static', filename="profile_pics/XL.jpg"),
+            'description': "is a senior majoring in computer engineering at The City College of New York. Previously he was interning as a Python programmer at Mini circuits company. He is beginning to pick up an interest in full-stack development at his senior design project at CCNY. He works as one of the backend developers for AdviseMe and hopes this platform will help students make better academic plans at CCNY.",
+            'git': 'https://github.com/linxunshan',
+            'linkedin': '',
+            'email': ''
+        },
+        {
+            'name': 'Rehman Arshad',
+            'image': url_for('static', filename="profile_pics/RA.png"),
+            'description': "is a senior majoring in Computer Science at The City College of New York. He has interned at various academic research institutions at The Groove school of Engineering, such as NOAA Crest (National Oceanic Atmospheric Administration) and Professor Tarek Sadawi. At NOAA Crest, he worked on data collection and analysis using data from the National Weather Service. With Professor Tarek Sadawi he worked on an IoT medical application that would enable remote patient monitoring as a research assistant. He was a part of CUNY Tech Prep cohort 5, a Full Stack program part of NYC Tech Talent Pipeline. Also, he has a fascination with mathematics and a passion for computer graphics and video game development. He will be graduating from The City College of New York in Fall 2021. He is one of the backend developers for AdviseMe and hopes this online platform can help the next generation of students entering CCNY.",
+            'git': 'https://github.com/rehman000',
+            'linkedin': '',
+            'email': ''
+        },
+    ]
+
+    return render_template("about.html", title="About", team=team)
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -70,7 +108,39 @@ def logout():
     flash('Your account is logged out', 'success')
     return redirect(url_for('home'))
 
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    # resize
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path) #save the picture in picture_path
+    return picture_fn
+
 @app.route('/account/', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename= "profile_pics/" + current_user.image_file)
+    return render_template("account.html", title="Account",
+                           image_file=image_file, form=form)
